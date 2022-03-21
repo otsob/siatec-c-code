c496ad5536c897fc06527049b887c8f0d0b1d1e0
Otso Björklund <otso.bjorklund@gmail.com>
2022-03-21 10:38:08 +0200

    Save criterion output to file

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -8,6 +8,6 @@ export BENCHMARK_DATASET_MAX_SIZE=10000
 export BENCHMARK_DATASET_STEP_SIZE=1000
 
 cd posemir/benchmark
-cargo criterion
+cargo criterion 2>&1 | tee ../../output/benchmark/criterion_output.txt
 cp -r target/criterion ../../output/benchmark/
 cd ../..
